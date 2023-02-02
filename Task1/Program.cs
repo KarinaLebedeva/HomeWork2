76e9faf07cb9f1f590fb7fb7c1c2433219d87bcf
Karina Lebedeva <karylebedeva@mail.ru>
2023-02-02 09:48:22 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int GetDel(int number)
+{
+    int numberOne = number / 100;
+    int numberTwo = number % 10;
+    int result = numberOne * 10 + numberTwo;
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{GetDel(number)}");
